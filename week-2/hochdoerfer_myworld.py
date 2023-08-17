2e71738a70cd2b5111b1efff141f8fe03267a732
Kyle Hochdoerfer <kjhoch2@gmail.com>
2023-08-17 10:57:42 -0400

    Week 2 complete

---
+++ week-2/hochdoerfer_myworld.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------
+# Title: hochdoerfer_myworld.py
+# Author: Kyle Hochdoerfer
+# Date: 08/15/23
+# Description: Code for outputting text using Python
+#----------------------------------------------------------
+
+#Declare a variable to hold a last name
+last_name = "Hochdoerfer"
+
+#Print text to the screen using the declared last name
+print("You are in " + last_name + "'s world!")
